6e3167cb5a07186b1cb7238d2a79dee62f75ce08
Polina Zolotukhina <polina@iwoork.com>
2017-05-16 11:46:47 +0100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+	{ title: "JavaScript book", pages: 111 },
+	{ title:"Harry Potter", pages: 900},
+	{ title: "Gone with wind", pages: 600 },
+	{ title: "Love book", pages: 3  }
+	]
+}
